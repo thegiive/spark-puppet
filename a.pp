e2aeec7867b608ad9cd14d8272783236db016108
Wisely Chen <thegiive@gmail.com>
2013-07-28 09:21:12 +0800

    add

--- a.pp
+++ a.pp
@@ -20,40 +20,44 @@ class install_java {
 }
 
 class hadoop{
+	$hadoop_location = "/home/thegiive/env" 
+	$hadoop_version = "hadoop-1.2.0" 
 	$hadoop_jobtracker_host = "localhost:54311" 
 	$hdfs_replication_value = 1 
+	$hadoop_tmp_dir = $hadoop_location+"/"+$hadoop_version+"/tmp/"
+	$hadoop_hdfs_host = "hdfs://localhost:54310" 
 	exec {"apps_wget":
 		command => "/usr/bin/wget  http://ftp.twaren.net/Unix/Web/apache/hadoop/common/hadoop-1.2.0/hadoop-1.2.0.tar.gz -O /tmp/hadoop.tgz",
 unless => "/bin/ls /tmp/hadoop.tgz",
 	}
 	exec {"untar hadoop":
 		command => "/bin/tar  zxvf  /tmp/hadoop.tgz",
-		cwd => "/home/thegiive/env" , 
+		cwd => "${hadoop_location}" , 
 		require => Exec["apps_wget"] , 
 	}
 	file{ "core-site.xml" : 
-		path => "/home/thegiive/env/hadoop-1.2.0/conf/core-site.xml" , 
+		path => "${hadoop_location}/${hadoop_version}/conf/core-site.xml" , 
 		source => "puppet:///hadoop/core-site.xml" , 
 		require => Exec["untar hadoop"] , 
 	}
 	file{ "mapred-site.xml" : 
-		path => "/home/thegiive/env/hadoop-1.2.0/conf/mapred-site.xml" , 
+		path => "${hadoop_location}/${hadoop_version}/conf/mapred-site.xml" , 
 		content => template("hadoop/mapred-site.xml"), 
 		require => Exec["untar hadoop"] , 
 	}
 	file{ "hdfs-site.xml" : 
-		path => "/home/thegiive/env/hadoop-1.2.0/conf/hdfs-site.xml" , 
+		path => "${hadoop_location}/${hadoop_version}/conf/hdfs-site.xml" , 
 		content => template("hadoop/hdfs-site.xml"), 
 		require => Exec["untar hadoop"] , 
 	}
 	file{ "hadoop-env.sh" : 
-		path => "/home/thegiive/env/hadoop-1.2.0/conf/hadoop-env.sh" , 
+		path => "${hadoop_location}/${hadoop_version}/conf/hadoop-env.sh" , 
 		source => "puppet:///hadoop/hadoop-env.sh" , 
 		require => Exec["untar hadoop"] , 
 	}
 	exec { "run hadoop": 
-		command => "/home/thegiive/env/hadoop-1.2.0/bin/start-all.sh", 
-		cwd => "/home/thegiive/env" , 
+		command => "${hadoop_location}/${hadoop_version}/bin/start-all.sh", 
+		cwd => "${hadoop_location}" 
 		require => Exec["untar hadoop"] ,
 	}
 
